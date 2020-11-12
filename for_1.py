1666e6a5a35d19067f43b07430defca204e214be
Cre-Lis <74320345+Cre-Lis@users.noreply.github.com>
2020-11-12 10:46:53 +0300

    Delete for_1.py

--- for_1.py
+++
@@ -1,7 +0,0 @@
-Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-======== RESTART: /Users/andrey_y/Desktop/Python/My_Progects/string_1.py =======
-0123456789
->>> 
